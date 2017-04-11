f37d69161a052f2b0ff71c4cd4c54c6836ac1dbc
Davut Ozcan <davutozcan87@gmail.com>
2017-04-11 11:29:50 +0300

    Hello world v1

--- camel_rest/src/main/java/com/cameltraining/ISampleService.java
+++ camel_rest/src/main/java/com/cameltraining/ISampleService.java
@@ -1,8 +1,20 @@
 package com.cameltraining;
 
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
 /**
  * Created by Lenovo on 4/11/2017.
  */
+@Path("/greeting")
 public interface ISampleService {
-    String info();
+    @GET
+    @Path("/hello/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    String greet(@PathParam("name") String name);
 }
--- camel_rest/src/main/java/com/cameltraining/SampleServiceImp.java
+++ camel_rest/src/main/java/com/cameltraining/SampleServiceImp.java
@@ -4,7 +4,8 @@ package com.cameltraining;
  * Created by Lenovo on 4/11/2017.
  */
 public class SampleServiceImp implements ISampleService{
-    public String info() {
-        return "Hello World";
+
+    public String greet(String name) {
+        return "Hello "+name;
     }
 }
