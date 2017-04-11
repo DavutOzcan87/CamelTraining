74452ae95b0b684c1e7802e391d94b44ee6890c2
Davut Ozcan <davutozcan87@gmail.com>
2017-04-11 08:22:17 +0300

    Draft 1

---
+++ camel_rest/src/main/java/com/cameltraining/ISampleService.java
@@ -0,0 +1,8 @@
+package com.cameltraining;
+
+/**
+ * Created by Lenovo on 4/11/2017.
+ */
+public interface ISampleService {
+    String info();
+}
---
+++ camel_rest/src/main/java/com/cameltraining/SampleServiceImp.java
@@ -0,0 +1,10 @@
+package com.cameltraining;
+
+/**
+ * Created by Lenovo on 4/11/2017.
+ */
+public class SampleServiceImp implements ISampleService{
+    public String info() {
+        return "Hello World";
+    }
+}
