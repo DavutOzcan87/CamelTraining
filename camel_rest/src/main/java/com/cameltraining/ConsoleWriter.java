7e01c53108caa7a29d420729fd06e15ca7d3c006
DavutOzcan <davutozcan87@gmail.com>
2017-04-11 23:55:46 +0300

    Received Rest message now forwarded to activemq then forwarded to console printer via blueprint.xml

--- camel_rest/src/main/java/com/cameltraining/ConsoleWriter.java
+++ camel_rest/src/main/java/com/cameltraining/ConsoleWriter.java
@@ -2,14 +2,39 @@ package com.cameltraining;
 
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Handler;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+import java.util.Map;
 
 
 /**
  * Created by Lenovo on 4/11/2017.
  */
 public class ConsoleWriter {
+    @Handler
     public void writeToConsole(Exchange exchange)
     {
-        System.out.println("Exchange received");
+        System.out.println("Exchange received:"+System.currentTimeMillis());
+        printHeaders(exchange.getIn().getHeaders());
+            System.out.println("--Received message--");
+            try {
+                System.out.println("body:"+exchange.getIn().getBody(String.class));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+    }
+
+    private void printHeaders(Map<String, Object> headers) {
+        try {
+            System.out.println("Headers");
+            for (String key:headers.keySet()){
+                System.out.println(key+"="+headers.get(key).toString());
+            }
+        }catch (Exception e)
+        {
+            //e.printStackTrace();
+        }
     }
 }
--- camel_rest/src/main/java/com/cameltraining/SampleServiceImp.java
+++ camel_rest/src/main/java/com/cameltraining/SampleServiceImp.java
@@ -2,6 +2,9 @@ package com.cameltraining;
 
 import static com.cameltraining.ServiceResponse.of;
 
+import javax.jms.*;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 /**
  * Created by Lenovo on 4/11/2017.
  */
@@ -11,7 +14,31 @@ public class SampleServiceImp implements ISampleService{
 
     public ServiceResponse greet(String name) {
         String result = "Hello "+name;
-        System.out.println("greet:"+result);
-        return of(result);
+        System.out.println("writing to amq:"+result);
+        try {
+            toAmq(result);
+            return of(result);
+        }catch (Exception e)
+        {
+            return of(e.getMessage());
+        }
+    }
+
+    private void toAmq(String result) throws JMSException
+    {
+            //created ConnectionFactory object for creating connection
+            ConnectionFactory factory = new ActiveMQConnectionFactory("Username", "Password", "tcp://localhost:61616");
+            //Establish the connection
+            Connection connection = factory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue("servicedata:greeting");
+            //Added as a producer
+            javax.jms.MessageProducer producer = session.createProducer(queue);
+            // Create and send the message
+            TextMessage msg = session.createTextMessage();
+            msg.setText(result);
+            producer.send(msg);
+
+
     }
 }
