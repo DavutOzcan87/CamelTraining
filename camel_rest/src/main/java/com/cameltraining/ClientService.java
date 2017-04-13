f1866fdc3dea235891aa1a1cdabf61661ba4c2cd
Davut Ozcan <davutozcan87@gmail.com>
2017-04-13 11:09:39 +0300

    Rest Client Working Version

--- camel_rest/src/main/java/com/cameltraining/ClientService.java
+++ camel_rest/src/main/java/com/cameltraining/ClientService.java
@@ -8,14 +8,13 @@ import java.awt.*;
  * Created by Lenovo on 4/12/2017.
  */
 @Consumes(value = "application/json")
-//@Produces(value = "application/xml,application/json")
+@Produces(value = "application/xml,application/json")
 public interface ClientService {
 
     @GET
     @Path("/ping/{msg}")
-    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
-    public ClientResponse ping(@PathParam("msg") String msg);
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public String ping(@PathParam("msg") String msg);
 //    {
 //        System.out.println("--Ping method--");
 //        if(response != null)
